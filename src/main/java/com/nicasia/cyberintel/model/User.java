2e6b684c1d0f989c8c86911e2cce1d6c90f183d7
101-Snaps <makhafola.101snaps@gmail.com>
2026-04-27 08:30:57 +0200

    Add JsonProperty annotation to staffCode field

--- src/main/java/com/nicasia/cyberintel/model/User.java
+++ src/main/java/com/nicasia/cyberintel/model/User.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 @Table(name = "users", indexes = {
@@ -39,8 +40,9 @@ public class User {
     private LocalDateTime lastLogin;
     private LocalDateTime createdAt;
 
-    @Transient
-    private String staffCode;
+   @Transient
+@JsonProperty("staffCode")
+private String staffCode;
 
     @PrePersist
     public void prePersist() {
