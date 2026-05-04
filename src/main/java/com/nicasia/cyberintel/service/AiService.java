15fefd5c167d2ece7ad4b12bf8c6d426e3fc2075
101-Snaps <makhafola.101snaps@gmail.com>
2026-05-04 20:50:58 +0200

    Change AI_URL to use environment variable

--- src/main/java/com/nicasia/cyberintel/service/AiService.java
+++ src/main/java/com/nicasia/cyberintel/service/AiService.java
@@ -11,7 +11,8 @@ import java.util.Map;
 public class AiService {
 
     private static final Logger logger = LoggerFactory.getLogger(AiService.class);
-    private static final String AI_URL = "https://ai-services-nkbg.onrender.com/predict-threat";
+    private static final String AI_URL = System.getenv().getOrDefault(
+    "AI_SERVICE_URL", "https://your-ai-service.onrender.com") + "/predict-threat";
 
     private final RestTemplate restTemplate = new RestTemplate();
 
